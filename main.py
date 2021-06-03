007062fcff55b2d2caa04f5cb6efd50605d2dd99
Andele Swierstra <andele.rien@gmail.com>
2021-06-03 15:18:47 +0200

    Added flake8 rules file, fixed code style, added initial data preprocessing draft

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ parser.add_argument("embeddings", type=Path, help="Embeddings directory")
 parser.add_argument("data", type=Path, help="Input files directory")
 parser.add_argument("--train_val_test", type=Path, help="Path to train val test splits")
 
+
 def preprocess(train_df, val_df, test_df):
-    
+
     ### Process data logics and methods
     pass
 
@@ -19,14 +20,15 @@ def run_model():
 
 def main():
     args = parser.parse_args()
-    
+
     if args.train_val_test:
         train_df, val_df, test_df = load_split_data(args.train_val_test)
-        
+
     else:
         true_df, fake_df = initial_load_data(path=args.data)
         train_df, val_df, test_df = split_data(true_df, fake_df)
         save_split_data(train_df, val_df, test_df, "data\\splitted")
-        
+
+
 if __name__ == "__main__":
     main()
