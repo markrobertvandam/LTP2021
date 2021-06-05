77d05a9aea42a6d1b4a72c834f110779e6a1df69
Andele Swierstra <andele.rien@gmail.com>
2021-06-05 19:02:42 +0200

    Added preprocessing with file saving

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,16 @@
 import numpy as np
 import pandas as pd
+from tap import Tap
 from fasttext import load_model
 from pathlib import Path
 from spacy import load
+from utils import initial_load_data, split_data, check_download_embeddings
+
+
+class ArgumentParser(Tap):
+    data_dir: str = "data"  # Directory that holds True.csv and Fake.csv
+    embedding_type: str = "text"  # Embedding type, 'text' or 'title'
+    save_path: Path = Path("data/embeddings.npz")  # Path to save the embeddings to
 
 
 class ArticleEmbeddings:
@@ -42,3 +50,32 @@ class ArticleEmbeddings:
 
     def title_embeddings(self, df: pd.DataFrame) -> np.ndarray:
         return np.array([self.ft.get_sentence_vector(title) for title in df["title"]])
+
+
+if __name__ == "__main__":
+    ap = ArgumentParser()
+    args = ap.parse_args()
+    true_df, fake_df = initial_load_data(args.data_dir)
+    train_df, val_df, test_df = split_data(true_df, fake_df)
+
+    check_download_embeddings()
+    article_embeddings = ArticleEmbeddings(ft_path="data\\embeddings\\cc.en.300.bin")
+    if args.embedding_type == "text":
+        res_train_embeddings = article_embeddings.text_embeddings(train_df)
+        res_val_embeddings = article_embeddings.text_embeddings(val_df)
+        res_test_embeddings = article_embeddings.text_embeddings(test_df)
+    elif args.embedding_type == "title":
+        res_train_embeddings = article_embeddings.title_embeddings(train_df)
+        res_val_embeddings = article_embeddings.title_embeddings(val_df)
+        res_test_embeddings = article_embeddings.title_embeddings(test_df)
+
+    with open(args.save_path) as out_file:
+        np.savez(
+            out_file,
+            X_train=res_test_embeddings,
+            y_train=train_df["label"],
+            X_val=res_test_embeddings,
+            y_val=val_df["label"],
+            X_test=res_test_embeddings,
+            y_test=test_df["label"],
+        )
